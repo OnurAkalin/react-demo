e2497c5f8390abe12e0bae26a303ab854e035d6e
Onur Akalın <onurakalin96@gmail.com>
2026-01-04 01:25:41 +0300

    .

--- eslint.config.js
+++ eslint.config.js
@@ -2,28 +2,28 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import {defineConfig, globalIgnores} from 'eslint/config'
 
-export default defineConfig([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
-    },
-    rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-    },
-  },
-])
+export default defineConfig([globalIgnores(['dist']),
+
+    js.configs.recommended,
+
+    {
+        files: ['**/*.{js,jsx}'],
+
+        languageOptions: {
+            ecmaVersion: 'latest', sourceType: 'module', globals: globals.browser, parserOptions: {
+                ecmaFeatures: {jsx: true},
+            },
+        },
+
+        plugins: {
+            'react-hooks': reactHooks, 'react-refresh': reactRefresh,
+        },
+
+        rules: {
+            ...reactHooks.configs.recommended.rules,
+            'react-refresh/only-export-components': ['warn', {allowConstantExport: true},],
+            'no-unused-vars': ['error', {varsIgnorePattern: '^[A-Z_]'}],
+        },
+    },])
